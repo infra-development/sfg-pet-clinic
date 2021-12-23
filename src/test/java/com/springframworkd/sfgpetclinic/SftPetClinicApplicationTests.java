3ad11d2f7cf87ad2396b830fe0bc5ceb97e9c91e
Haresh Bhuriya <hareshybhuriya@gmail.com>
2021-12-23 22:15:40 +0530

    Add project

---
+++ src/test/java/com/springframworkd/sfgpetclinic/SftPetClinicApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springframworkd.sfgpetclinic;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SftPetClinicApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
