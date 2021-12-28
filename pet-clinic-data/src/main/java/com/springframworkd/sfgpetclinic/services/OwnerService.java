bea74145d5a4f1704f62d4bd802955b13291d906
Haresh Bhuriya <hareshybhuriya@gmail.com>
2021-12-28 20:30:51 +0530

    SPC-2 Creating Interfaces for Pet Clinic Service

---
+++ pet-clinic-data/src/main/java/com/springframworkd/sfgpetclinic/services/OwnerService.java
@@ -0,0 +1,16 @@
+package com.springframworkd.sfgpetclinic.services;
+
+import com.springframworkd.sfgpetclinic.model.Owner;
+
+import java.util.Set;
+
+public interface OwnerService {
+
+    Owner findByName(String name);
+
+    Owner findById(Long id);
+
+    Owner save(Owner owner);
+
+    Set<Owner> findAll();
+}
