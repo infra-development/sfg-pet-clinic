21d06f8bc5cddd1e1e555e3be8224cfcf34d6220
Haresh Bhuriya <hareshybhuriya@gmail.com>
2022-01-02 14:28:53 +0530

    it's really good welcome

--- pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
-    // Another welcome
+    // Good Welcome
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
