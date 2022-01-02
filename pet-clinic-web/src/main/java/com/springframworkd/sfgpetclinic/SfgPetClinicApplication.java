9bcba75875cf6a405dbd86cf44c79dc6c8ff2051
Haresh Bhuriya <hareshybhuriya@gmail.com>
2022-01-02 22:24:08 +0530

    SPC-13: #Done This is final test

--- pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SfgPetClinicApplication {
 
-    // this is test
+    // final test
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
