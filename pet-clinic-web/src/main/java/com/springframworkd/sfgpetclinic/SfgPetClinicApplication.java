f87f766496e6afe7c570a04146c87158960be125
Haresh Bhuriya <hareshybhuriya@gmail.com>
2022-01-02 13:29:25 +0530

    SPC-7 hello java

--- pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
+++ pet-clinic-web/src/main/java/com/springframworkd/sfgpetclinic/SfgPetClinicApplication.java
@@ -8,7 +8,6 @@ public class SfgPetClinicApplication {
 
     // testing tool
     // another testing1
-    // new commit
     public static void main(String[] args) {
         SpringApplication.run(SfgPetClinicApplication.class, args);
     }
