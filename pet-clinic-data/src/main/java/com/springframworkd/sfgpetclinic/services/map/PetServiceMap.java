c873a576351f47c5b34114451466c8ea2d721d40
Haresh Bhuriya <hareshybhuriya@gmail.com>
2022-01-17 22:24:25 +0530

    adding bootstrap data

--- pet-clinic-data/src/main/java/com/springframworkd/sfgpetclinic/services/map/PetServiceMap.java
+++ pet-clinic-data/src/main/java/com/springframworkd/sfgpetclinic/services/map/PetServiceMap.java
@@ -7,6 +7,7 @@ import java.util.Set;
 
 public class PetServiceMap extends AbstractMapService<Pet, Long> implements CrudService<Pet, Long> {
 
+
     public Set<Pet> findAll() {
         return super.findAll();
     }
